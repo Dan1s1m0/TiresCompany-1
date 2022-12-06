5c9b639555d1787b2dec38718fbde1b7e63db36a
rotoyouoi <rotoyouoi@yandex.ru>
2022-12-06 15:08:38 +0500

    Добавьте файлы проекта.

---
+++ TiresCompany/View/UpdateProductPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TiresCompany.Model;
+
+namespace TiresCompany.View
+{
+    /// <summary>
+    /// Логика взаимодействия для UpdateProductPage.xaml
+    /// </summary>
+    public partial class UpdateProductPage : Page
+    {
+        Core db = new Core();
+        Product product;
+        public UpdateProductPage()
+        {
+            InitializeComponent();
+            TypeComboBox.ItemsSource = db.context.ProductType.ToList();
+            product = new Product();
+            DataContext = product;
+        }
+        public UpdateProductPage(Product product)
+        {
+            InitializeComponent();
+            TypeComboBox.ItemsSource = db.context.ProductType.ToList();
+            DataContext = product;
+        }
+
+        private void ChangeImageButtonClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog winDialog = new OpenFileDialog();
+            if (winDialog.ShowDialog()==true)
+            {
+                Uri sourse = new Uri(winDialog.FileName);
+                ProductImage.Source = new BitmapImage(sourse);
+            }
+        }
+    }
+}
